a1a7cc3ac7a35d4fc6853d67d8b07792463dfed1
SnehaDhanuka05 <10c.snehadhanuka@gmail.com>
2026-06-23 21:12:36 +0530

    updating cors config

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://space-ops-phi.vercel.app/",
+        "https://space-ops-phi.vercel.app",
         "http://localhost:3000"
     ],
     allow_credentials=True,
